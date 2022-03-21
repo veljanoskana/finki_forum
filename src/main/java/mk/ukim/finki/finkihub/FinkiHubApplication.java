d9c4d588e8f92f6b34c8c3cbf6e1b024965be195
Ana Veljanoska <anaveljanoska@yahoo.com>
2022-03-21 22:29:00 +0100

    test commit

--- src/main/java/mk/ukim/finki/finkihub/FinkiHubApplication.java
+++ src/main/java/mk/ukim/finki/finkihub/FinkiHubApplication.java
@@ -3,6 +3,9 @@ package mk.ukim.finki.finkihub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.servlet.annotation.WebServlet;
+
+
 @SpringBootApplication
 public class FinkiHubApplication {
 

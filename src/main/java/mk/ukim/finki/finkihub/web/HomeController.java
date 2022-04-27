061a53a7554259dce682cb5bab1cc25ff9687144
Ana Veljanoska <anaveljanoska@yahoo.com>
2022-04-27 14:32:08 +0200

    initial frontend design

--- src/main/java/mk/ukim/finki/finkihub/web/HomeController.java
+++ src/main/java/mk/ukim/finki/finkihub/web/HomeController.java
@@ -1,17 +1,29 @@
 package mk.ukim.finki.finkihub.web;
-import mk.ukim.finki.finkihub.models.Student;
+import mk.ukim.finki.finkihub.models.Course;
+import mk.ukim.finki.finkihub.service.CourseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping({"/", "/home"})
 public class HomeController {
+    private final CourseService courseService;
+
+    public HomeController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @GetMapping
     public String getHomePage(Model model) {
-        return "frontend";
+        List<Course> courses = this.courseService.findAll();
+        model.addAttribute("courses", courses);
+        return "allCourses";
+//        model.addAttribute("bodyContent", "allCourses");
+//        return "master-template";
     }
 
 }

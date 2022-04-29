75a9a80d0594cd7bc4373f01b8e2e8d06a2db6b1
Ana Veljanoska <anaveljanoska@yahoo.com>
2022-04-29 12:12:20 +0200

    rollback security changes

--- src/main/java/mk/ukim/finki/finkihub/FinkiHubApplication.java
+++ src/main/java/mk/ukim/finki/finkihub/FinkiHubApplication.java
@@ -3,8 +3,6 @@ package mk.ukim.finki.finkihub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.servlet.annotation.WebServlet;
 
@@ -15,9 +13,5 @@ public class FinkiHubApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinkiHubApplication.class, args);
     }
-
-//    @Bean
-//    PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder(10);
-//    }
+    
 }
